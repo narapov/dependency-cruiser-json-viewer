b53564721610b4e2928ccbd6c76c6096d0511b67
Nikolay Arapov <nikolayarapov@MacBook-Pro-Nikolay.local>
2026-06-30 21:33:04 +0300

    ci: add release-it

---
+++ .release-it.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'release-it';
+
+export default {
+  git: {
+    commitMessage: 'chore: release v${version}',
+    requireBranch: 'main',
+    requireCleanWorkingDir: true,
+    tagName: 'v${version}',
+  },
+  github: {
+    release: true,
+    releaseName: 'v${version}',
+  },
+  npm: {
+    publish: true,
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'conventionalcommits',
+      infile: 'CHANGELOG.md',
+    },
+  },
+  hooks: {
+    'before:init': ['npm run lint', 'npm run format:check', 'npm run test', 'npm run depcruise', 'npm run build'],
+  },
+} satisfies Config;
